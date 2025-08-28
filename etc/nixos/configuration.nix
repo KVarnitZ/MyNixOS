ce69ac8bdaddeab7112b4707447aaf1bd23e6346
KVarnitZ <90161712+KVarnitZ@users.noreply.github.com>
2025-08-28 08:29:57 +0300

    Оновлена configuration.nix через темку
    Тепер temka.nix інтегрується в nix-store

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -2,26 +2,44 @@
 
 let
 
-  # Завантаження HomeManager  
+  # Завантаження HomeManager
   home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+
+  # Можливість вантажити пакети з нестабільного аналу
+  unstable = import <nixos-unstable> { config = config.nixpkgs.config; };
 
   # Завантаження konfihy.nix
   konfihyNix = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/KVarnitZ/MyNixOS/main/etc/nixos/konfihy.nix";
-    # Цей sha256 дійсний станом на 2025-07-12
-    # sha256 отримується через "nix-prefetch-url --type sha256 (посилання) | xargs nix hash to-sri --type sha256"
-    sha256 = "ВИКОРИСТАЙТЕ КОМАНДУ ДЛЯ ОТРИМАННЯ sha256";
+    # Цей sha256 дійсний станом на 2025-06-14
+    # sha256 отримується через "nix-prefetch-url --type sha256 https://raw.githubusercontent.com/KVarnitZ/MyNixOS/main/etc/nixos/konfihy.nix | xargs nix hash to-sri --type sha256"
+    sha256 = "sha256-/q0obG84BPnYO23b8AnINuNwwBS2JbsEsQGpLfdOn/4=";
   };
 
   # Завантаження zastosunky.nix
   zastosunkyNix = builtins.fetchurl {
     url = "https://raw.githubusercontent.com/KVarnitZ/MyNixOS/main/etc/nixos/zastosunky.nix";
-    # Цей sha256 дійсний станом на 2025-07-12
-    # sha256 отримується через "nix-prefetch-url --type sha256 (посилання) | xargs nix hash to-sri --type sha256"
-    sha256 = "ВИКОРИСТАЙТЕ КОМАНДУ ДЛЯ ОТРИМАННЯ sha256";
+    # Цей sha256 дійсний станом на 2025-06-14
+    # sha256 отримується через "nix-prefetch-url --type sha256 https://raw.githubusercontent.com/KVarnitZ/MyNixOS/main/etc/nixos/zastosunky.nix | xargs nix hash to-sri --type sha256"
+    sha256 = "sha256-uakUtxbYa4LDZq74t6z198lK8mdePFgpqzZhOdOA95w=";
   };
-in
 
+  # Завантаження temka.nix
+  temkaNix = builtins.fetchurl {
+    url = https://raw.githubusercontent.com/KVarnitZ/MyNixOS/refs/heads/main/etc/nixos/temka/temka.nix;
+    # Цей sha256 дійсний станом на 2025-06-14
+    # sha256 отримується через "nix-prefetch-url --type sha256 https://raw.githubusercontent.com/KVarnitZ/MyNixOS/main/etc/nixos/temka/temka.nix | xargs nix hash to-sri --type sha256"
+    sha256 = "sha256-P9CUI+2LPP6b//gEW/245q4dx/kKrWIJzNHOiMDAu54=";
+  };
+
+  #myNixOSRepo = builtins.fetchTarball {
+    #url = "https://github.com/KVarnitZ/MyNixOS/archive/main.tar.gz";
+    # Важливо! Вам потрібно буде оновити цей sha256 щоразу, коли ви вносите зміни до репозиторію.
+    # Для отримання нового хешу виконайте в терміналі:
+    # nix-prefetch-url --type sha256 https://github.com/KVarnitZ/MyNixOS/archive/main.tar.gz | xargs nix hash to-sri --type sha256
+    #sha256 = "sha256-cBtgym/VzVQP3kbCQWgLLbnpE+KSz50m5bVhOSZFe3o=";
+  #};
+in
 {
 
   environment.etc = {
@@ -32,9 +50,15 @@ in
     };
 
     # Додавання zastosunky.nix до /etc/nixos/
-    "nixos/zastosunky.nix" = {
+   "nixos/zastosunky.nix" = {
       source = zastosunkyNix;
       # copyFrom = zastosunkyNix;
+    };
+   
+   # Додавання temka.nix до /etc/nixos/temka/
+   "nixos/temka/temka.nix" = {
+      source = temkaNix;
+      # copyFrom = temkaNix;
     };
   };
 
@@ -43,24 +67,25 @@ in
       #(import "${home-manager}/nixos")
       konfihyNix # Імпорт з nix-store (копія зберігається в /etc/nixos)
       zastosunkyNix # Імпорт з nix-store (копія зберігається в /etc/nixos)
+      temkaNix # # Імпорт з nix-store (копія зберігається в /etc/nixos/temka)
       ./hardware-configuration.nix
-      #./test.nix
+      ./shryftyky.nix
     ];
 
-  # Bootloader.
+  # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "KVarnitZ"; # Define your hostname.
 
-  # Enable networking
+  # Інтернат
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
+  # Де я?
   time.timeZone = "Europe/Kyiv";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  # Мовне питання
+  i18n.defaultLocale = "uk_UA.UTF-8"; # en_US.UTF-8
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "uk_UA.UTF-8";
@@ -74,13 +99,21 @@ in
     LC_TIME = "uk_UA.UTF-8";
   };
 
-  # Configure keymap in X11
+  # Звук та екран
+  services.pipewire = {
+    enable = true;
+    wireplumber.enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
+
+  # Клавіши тикати X11
   services.xserver.xkb = {
     layout = "us,ua";
     variant = "";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # Користувачі - ригачі
   users.users.kvarnitz = {
     isNormalUser = true;
     description = "KVarnitZ";
@@ -88,8 +121,9 @@ in
     packages = with pkgs; [];
   };
 
-  system.stateVersion = "25.05"; # Теперішня версія
+  system.stateVersion = "25.05"; # Теперішня версія системи
 
+  # САМОПИС БЛЯЯЯЯЯЯЯТЬ
   environment.systemPackages = with pkgs; [
     # Cumshot
     grim # Скрінер
@@ -100,7 +134,6 @@ in
     # Хуй
     wofi # Меню програм
     nemo # Файловий менеджер
-    kdePackages.dolphin # Файловий менеджер 2
     chromium # Браузер
     vesktop # Діскорд
     element-desktop # Matrix 
@@ -109,8 +142,8 @@ in
     telegram-desktop # ФСБ транслятор
     qbittorrent # Торент
     protonvpn-gui # VPN
-    #davinci-resolve # Відео рекдактор
-    kdePackages.kdenlive # Ще один відео редактор
+    davinci-resolve # Відео рекдактор
+    kdePackages.kdenlive # Відео редактор 2
     gimp-with-plugins # Фото редактор
     audacity # Аудіо редактор
     obs-studio # Запис відео
@@ -129,6 +162,9 @@ in
     lm_sensors # Температура пк
     # Гратися
     steam # Вентиль в шапці
+    protonup-ng # Кастомні протони для стіму
+    wine
+    gamescope # Маніпуляції з іграми
     heroic # Інші лаунчери в одному
     lutris # Інші лаунчери в одному 2
     prismlauncher # Майнкрафт
@@ -149,14 +185,34 @@ in
       "logmein-hamachi"
     ];
 
+  # Дозвіл (1000-7)*2 пакетів
+  nixpkgs.overlays = [
+    (self: super: {
+      davinci-resolve = unstable.davinci-resolve;
+    })
+  ];
+
+  services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
   # Експерементальна ерекція
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Усяка херь, аби композитор робив
+  # Усяка херь, аби композитори робили
   services.displayManager.sddm.enable = true;
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+  };
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
   };
 
   # Програмний угар
@@ -194,12 +250,12 @@ in
 
   # Харчок у список дисків
   fileSystems."/mnt/Smitnyk" = {
-    device = "/dev/disk/by-uuid/ВСТАВТЕ UUID ВАШОГО ДИСКУ"; # Вказування точного диску, інакше вони будуть рандомно єднатися
+    device = "/dev/disk/by-uuid/c0d8bc1f-f271-45d0-9bbe-a2e44eb940bc"; # Вказування точного диску, інакше вони будуть рандомно єднатися
     #device = "/dev/sda1";
     fsType = "ext4";
   };
   fileSystems."/mnt/Zvalysche" = {
-    device = "/dev/disk/by-uuid/ВСТАВТЕ UUID ВАШОГО ДИСКУ"; # Вказування точного диску, інакше вони будуть рандомно єднатися
+    device = "/dev/disk/by-uuid/c67938c7-35a8-4ae8-8caf-2ce0f23b993d"; # Вказування точного диску, інакше вони будуть рандомно єднатися
     #device = "/dev/sdb1";
     fsType = "ext4";
   };
