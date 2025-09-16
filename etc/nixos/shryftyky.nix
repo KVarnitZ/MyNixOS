a8145f7661d21978529a427afae0195f54cd3220
KVarnitZ <90161712+KVarnitZ@users.noreply.github.com>
2025-09-16 19:58:46 +0000

    Спроба переносу у flake 3 shryftyky.nix
    Додавання шрифтів тепер має відбуватися всесистемно

--- etc/nixos/shryftyky.nix
+++ etc/nixos/shryftyky.nix
@@ -1,22 +1,13 @@
 { pkgs, inputs, ... }:
-
-let
+{
   # Створення пакету неприємних написів
   shryftykys = pkgs.stdenv.mkDerivation rec {
     pname = "shryftykys";
     version = "1.0";
-    src = "${inputs.my-nixos-repo}/etc/nixos/fonts";
-    # Утворення неприємних написів
+    src = "${inputs.my-nixos-repo}/home/kvarnitz/.local/share/fonts";
     installPhase = ''
       mkdir -p $out/share/fonts
       cp -R ./* $out/share/fonts/
     '';
   };
-in
-{
-  # Увімкнення неприємних написів
-  fonts.fontconfig.enable = true;
-
-  # Додавання пакета неприємних написів
-  home.packages = [ shryftykys ];
 }
