de4051145fab83fa6ec64be50ec029dc46e9541c
KVarnitZ <90161712+KVarnitZ@users.noreply.github.com>
2025-09-16 19:52:34 +0000

    Перенос у flake shryftyky.nix
    Шрифти мають бути пакетом та вставляти в систему

--- etc/nixos/shryftyky.nix
+++ etc/nixos/shryftyky.nix
@@ -1,17 +1,22 @@
-{ config, pkgs, ... }:
+{ pkgs, inputs, ... }:
 
+let
+  # Створення пакету неприємних написів
+  shryftykys = pkgs.stdenv.mkDerivation rec {
+    pname = "shryftykys";
+    version = "1.0";
+    src = "${inputs.my-nixos-repo}/etc/nixos/fonts";
+    # Утворення неприємних написів
+    installPhase = ''
+      mkdir -p $out/share/fonts
+      cp -R ./* $out/share/fonts/
+    '';
+  };
+in
 {
-
-  # Увімкнення шрифтиків
+  # Увімкнення неприємних написів
   fonts.fontconfig.enable = true;
 
-  # Додавання шрифтиків у систему всеосяжно
-  fonts.fontconfig.localConf = ''
-    <?xml version="xuj"?>
-    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-    <fontconfig>
-      <dir>.../home/kvarnitz/.local/share/fonts</dir> # Шлях до шрифтиків
-      </fontconfig>
-  '';
-
+  # Додавання пакета неприємних написів
+  home.packages = [ shryftykys ];
 }
