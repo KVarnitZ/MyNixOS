4d1995ca983122274196c3849b10753795095ba8
KVarnitZ <90161712+KVarnitZ@users.noreply.github.com>
2025-09-16 20:18:03 +0000

    Очередна спроба перенести у flake shryftyky.nix
    Інакший підхід до створення пакета

--- etc/nixos/shryftyky.nix
+++ etc/nixos/shryftyky.nix
@@ -1,13 +1,12 @@
 { pkgs, inputs, ... }:
-{
-  # Створення пакету неприємних написів
-  shryftykys = pkgs.stdenv.mkDerivation rec {
-    pname = "shryftykys";
-    version = "1.0";
-    src = "${inputs.my-nixos-repo}/home/kvarnitz/.local/share/fonts";
-    installPhase = ''
-      mkdir -p $out/share/fonts
-      cp -R ./* $out/share/fonts/
-    '';
-  };
-}
+
+# Створення пакету неприємних написів
+ pkgs.stdenv.mkDerivation rec {
+   pname = "my-custom-fonts";
+   version = "1.0";
+   src = "${inputs.my-nixos-repo}/etc/nixos/fonts";
+   installPhase = ''
+     mkdir -p $out/share/fonts
+     cp -R ./* $out/share/fonts/
+   '';
+ }
