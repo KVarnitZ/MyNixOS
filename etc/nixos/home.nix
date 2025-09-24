294aba4d2976d4c90c593138c12bccc4b681279f
KVarnitZ <90161712+KVarnitZ@users.noreply.github.com>
2025-09-24 02:28:39 +0000

    Створення тла через home.nix
    Тепер тло створюється у потрібній папці

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -13,4 +13,7 @@ home.stateVersion = "25.05";
     source = "${inputs.my-nixos-repo}/home/kvarnitz/.local/share/fonts";
     recursive = true;
   };
+  home.file.".config/hypr/Wallpaper.jpg" = {
+    source = "${inputs.my-nixos-repo}/home/kvarnitz/.config/hypr/Wallpaper.jpg";
+  };
 }
