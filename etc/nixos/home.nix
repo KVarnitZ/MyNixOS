80451a16179e7e383d4dd80b52abe850353e5e41
KVarnitZ <90161712+KVarnitZ@users.noreply.github.com>
2025-10-31 06:21:02 +0000

    Додавання шрифта як основного home.nix
    Тепер nihonium113 має бути основним шрифтом всієї системи

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -5,10 +5,6 @@ home.username = "kvarnitz";
 home.homeDirectory = "/home/kvarnitz";
 home.stateVersion = "25.05";
 
-  fonts.fontconfig = {
-    enable = true;
-  };
-
   home.file.".local/share/fonts" = {
     source = "${inputs.my-nixos-repo}/home/kvarnitz/.local/share/fonts";
     recursive = true;
@@ -16,4 +12,35 @@ home.stateVersion = "25.05";
   home.file.".config/hypr/Wallpaper.jpg" = {
     source = "${inputs.my-nixos-repo}/home/kvarnitz/.config/hypr/Wallpaper.jpg";
   };
+
+  fonts.fontconfig = {
+    enable = true;
+    localConf = ''
+      <?xml version="1.0"?>
+      <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+      <fontconfig>
+        <alias>
+          <family>sans-serif</family>
+          <prefer><family>Nihonium113</family></prefer>
+        </alias>
+        <alias>
+          <family>serif</family>
+          <prefer><family>Nihonium113</family></prefer>
+        </alias>
+        <alias>
+          <family>monospace</family>
+          <prefer><family>Nihonium113</family></prefer>
+        </alias>
+      </fontconfig>
+    '';
+  };
+
+gtk = {
+    enable = true;
+    font = {
+      name = "Nihonium113 Regular";
+      size = 12;
+    };
+  };
+
 }
